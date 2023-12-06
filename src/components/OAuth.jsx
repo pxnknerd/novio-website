3979e8dd6b98eb56847e3d098edca5baa95e9086
pxnknerd <makdad35@gmail.com>
2023-12-06 01:29:52 +0100

    complete the sign in functionality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,36 +1,36 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
-import { doc, getDoc, setDoc, serverTimestamp } from 'firebase/firestore';
-import React from 'react';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, setDoc, serverTimestamp } from "firebase/firestore";
+import React from "react";
 import { FaGoogle } from "react-icons/fa";
 import { toast } from "react-toastify";
-import {db} from "../firebase"
-import { useNavigate } from 'react-router-dom';
+import {db} from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 export default function OAuth() {
   const navigate = useNavigate()
   async function onGoogleClick(){
     try {
-      const auth = getAuth()
-      const provider = new GoogleAuthProvider()
-      const result = await signInWithPopup(auth, provider)
-      const user = result.user
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
 
       //check for the user
 
-      const docRef = doc(db, "users", user.uid)
-      const docSnap = await getDoc(docRef)
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
 
-      if(docSnap.exists()){
+      if(docSnap.exists()) {
         await setDoc(docRef, {
           name: user.displayName,
           email: user.email,
           timestamp: serverTimestamp(),
-       } );
+       });
       }
 
-      navigate("/")
+      navigate("/home");
     } catch (error) {
-      toast.error("Could not authorize with Google")
+      toast.error("Could not authorize with Google");
 
     }
 
